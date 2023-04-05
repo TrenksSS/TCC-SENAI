65111fbf3f65720c841824180d50e89f2bdb9419
GleidsonSousa <gleidson2090@gmail.com>
2023-04-05 11:20:58 -0300

    DIA 5

---
+++ backEnd/server.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+const PORT = process.env.PORT || 3000
+const express = require('express')
+const cors = require('cors')
+
+const router = require('./src/routes/router')
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+app.use(router)
+
+app.listen(PORT, () => {
+    console.log("App ON porta:" + PORT)
+})
