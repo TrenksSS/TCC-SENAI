b38a45f2f0d7a1e86e2724c2e8da81a5353ad813
GleidsonSousa <gleidson2090@gmail.com>
2023-04-18 11:20:41 -0300

    new att

---
+++ frontEnd/paginas/login/login.js
@@ -0,0 +1,44 @@
+const uriLogin = "http://localhost:2550/passageiros/login"
+const email = document.querySelector("#email")
+const psw = document.querySelector("#psw")
+const alertSpan = document.querySelector("#spanAlert")
+
+const login = (err) => {
+    // email.value = "hobertAlberto@gmail.com"
+    // psw.value = "1234"
+    let usuario = {
+        "email": email.value,
+        "senha":psw.value 
+    }
+
+    fetch(uriLogin, {
+        'method':'POST',
+        'headers': {
+            'Content-Type':'application/json'
+        },
+        body: JSON.stringify(usuario)
+
+    }).then(response => { return response.status, response.json() })
+    .then(info => {
+        console.log(info)
+        if(info != null ) {
+
+            if(info.erro == "Senha inválida"){
+                alertSpan.classList.remove('model')
+                alertSpan.innerHTML = info.erro
+                setTimeout(() => {alertSpan.classList.add('model')}, 750);
+            } else if(info.erro == "Passageiro não encontrado"){
+                alertSpan.classList.remove('model')
+                alertSpan.innerHTML = info.erro
+                setTimeout(() => {alertSpan.classList.add('model')}, 750);
+            }else if(info.validation == true){
+                localStorage.clear();
+                localStorage.setItem('usuario',JSON.stringify({"role":info.role}));
+                console.log(localStorage)
+                window.location.href = "../dashboard/dashboard.html"
+            }
+        } else {
+            alert(' ❌ Erro no Login:' + info);
+        }
+    })
+}
